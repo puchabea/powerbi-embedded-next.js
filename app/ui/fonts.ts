5d63ff400fb671a205c79fe218d92e82e6132175
Beatriz Toledo <beatriz.toledo@ug.uchile.cl>
2026-01-15 21:05:32 -0300

    chore: add additional fonts

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,6 +1,18 @@
 import { Montserrat } from "next/font/google";
+import { Plus_Jakarta_Sans } from "next/font/google";
+import { Inter } from "next/font/google";
 
 export const montserrat = Montserrat({
   subsets: ["latin"],
   weight: ["400", "500", "600", "700"],
+});
+
+export const jakarta = Plus_Jakarta_Sans({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700", "800"],
+});
+
+export const inter = Inter({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700", "800"],
 });
