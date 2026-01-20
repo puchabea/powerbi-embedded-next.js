ff94b2b789ca70947386014865c73c8f5f4cd8c7
Beatriz Toledo <beatriz.toledo@ug.uchile.cl>
2026-01-20 17:33:05 -0300

    feat: add users page

---
+++ app/dashboard/users/page.tsx
@@ -0,0 +1,13 @@
+export default function UserPage() {
+  return (
+    <div>
+      <h1 className="text-2xl font-semibold tracking-tight">
+        Usuarios
+      </h1>
+
+      <p className="mt-2 max-w-2xl text-zinc-600">
+        Aqui estarán los usuarios.
+      </p>
+    </div>
+  );
+}
